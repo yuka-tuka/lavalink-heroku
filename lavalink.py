41b06dc82a42b3b3e5aa18ee479437922d83b167
yuka-tuka <53022362+yuka-tuka@users.noreply.github.com>
2020-01-03 01:17:17 -0300

    Update lavalink.py

--- lavalink.py
+++ lavalink.py
@@ -5,7 +5,7 @@ from os import system, environ
 class lavalinkserver:
 
     def __init__(self):
-        self.download_command = "curl -s https://api.github.com/repos/Frederikam/Lavalink/releases/15030275 | grep browser_download_url | cut -d '\"' -f 4 | wget -qi -"
+        self.download_command = "curl -s https://api.github.com/repos/Frederikam/Lavalink/releases/18978720 | grep browser_download_url | cut -d '\"' -f 4 | wget -qi -"
         self.change_port = 'sed -i "s|DYNAMICPORT|$PORT|" application.yml'
         self.change_password = 'sed -i "s|DYNAMICPASSWORD|$PASSWORD|" application.yml'
         self.no_password = 'sed -i "s|DYNAMICPASSWORD|youshallnotpass|" application.yml'
